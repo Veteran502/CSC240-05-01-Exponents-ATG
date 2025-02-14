82360709c0d0d0383f6241ff41905f80e29b5738
Aaron Gill <AGILL4401@my.sullivan.edu>
2025-02-14 05:33:48 -0500

    Square works

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -27,19 +27,23 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
+            int num;
 
             // INPUT
+            num = Convert.ToInt32(UxInputTextBox.Text);
 
             // PROCESS
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
-
+            int squareAnswer = Square(num);
+            UxOutputLabel.Text = squareAnswer.ToString();
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
 
             // OUTPUT
             // set the output to visible
+            UxOutputLabel.Visible = true;
 
             // turn the Go button off
         }
@@ -47,12 +51,13 @@ namespace Exponents
         public int Square(int num)
         {
             // we will create this code in class
+            return num * num;
         }
 
-        public int Cube(int num)
-        {
-            // we will create this code in class
-        }
+        //public int Cube(int num)
+        //{
+        //    // we will create this code in class
+        //}
 
         private void xResetButton_Click(object sender, EventArgs e)
         {
@@ -62,6 +67,11 @@ namespace Exponents
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xInputTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
